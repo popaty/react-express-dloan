d2456a86bfda353d38a63206f27ba382297bccaa
petchza <anon.phaoinjun@gmail.com>
2019-11-07 14:48:06 +0700

    editing inquiry account reacord

--- client/src/App/pages/inqAccountingRecords/InquiryAccountingRecordsComponent.js
+++ client/src/App/pages/inqAccountingRecords/InquiryAccountingRecordsComponent.js
@@ -5,7 +5,6 @@ import utility from '../Utility.js';
 import SpinnerLoader from '../loading.js';
 import { Button, Col, Container, Form, FormGroup, Input, Label, Row } from 'reactstrap';
 
-
 class InquiryAccountingRecordsComponent extends Component {
     constructor(props) {
         super(props);
@@ -35,27 +34,177 @@ class InquiryAccountingRecordsComponent extends Component {
 
     Clicked(event) {
         event.preventDefault();
-        // console.log(this.state);
-        this.setState({ loading: true });
-        setTimeout(() => {
-            this.setState({ loading: false });
-            fetch('/api/inquiryAccountingRecord/' + this.state.account_number + "/" + this.state.account_sequence + "/"
-            + this.state.transaction_date + "/" + this.state.channel_post_date + "/" + this.state.job_id + "/" +
-            this.state.service,{})
-                .then(response => response.json())
-                .then(data => { 
-                    if (data.rs_body) {
-                        utility.clearSessionStorage("response_inquiryAccountingRecord");
-                        sessionStorage.setItem("response_inquiryAccountingRecord", JSON.stringify(data.rs_body));
-                        window.open('/iarSummary', '_self');
-                    } else {
-                        alert("error code : " + data.errors.map(error => error.error_code) + "\n"
-                            + "error desc : " + data.errors.map(error => error.error_desc) + "\n"
-                            + "error type : " + data.errors.map(error => error.error_type));
-                    }
+        console.log(this.state);
+        // this.setState({ loading: true });
+        // setTimeout(() => {
+        //     this.setState({ loading: false });
+        //     fetch('/api/inquiryAccountingRecord/' + this.state.account_number + "/" + this.state.account_sequence + "/"
+        //     + this.state.transaction_date + "/" + this.state.channel_post_date + "/" + this.state.job_id + "/" +
+        //     this.state.service,{})
+        //         .then(response => response.json())
+        //         .then(data => { 
+        //             if (data.rs_body) {
+        //                 utility.clearSessionStorage("response_inquiryAccountingRecord");
+        //                 sessionStorage.setItem("response_inquiryAccountingRecord", JSON.stringify(data.rs_body));
+        //                 window.open('/iarSummary', '_self');
+        //             } else {
+        //                 alert("error code : " + data.errors.map(error => error.error_code) + "\n"
+        //                     + "error desc : " + data.errors.map(error => error.error_desc) + "\n"
+        //                     + "error type : " + data.errors.map(error => error.error_type));
+        //             }
 
-                }).catch(error => console.log(error));
-         }, 1000);
+        //         }).catch(error => console.log(error));
+        //  }, 1000);
+
+        //mockdata
+        let data = {
+            "rs_body": {
+                "gl_entry_list": [
+                    {
+                        "transaction_id": "4S8UG08LOtEiLk--0-1F-Go-1",
+                        "transaction_sequence": 1,
+                        "transaction_datetime": "2019-10-30T15:26:05+07:00",
+                        "transaction_date": "2019-10-30",
+                        "channel_post_date": "2019-08-25",
+                        "datetime_stamp": "2019-11-06T14:51:20+07:00",
+                        "job_id": "762c31b2-359b-4301-b864-26bf9e53cdde",
+                        "account_branch": 1,
+                        "service_branch": 1,
+                        "cost_center": "1",
+                        "transaction_account_type": "AC",
+                        "dr_cr": "D",
+                        "gl_account_number": 1000001,
+                        "account_number": 60000000001,
+                        "account_sequence": 1,
+                        "before_prin_balance": 0.00,
+                        "transaction_amount": 100000.00,
+                        "after_prin_balance": 100000.00,
+                        "currency": "THB",
+                        "base_amount": 100000.00,
+                        "base_currency": "THB",
+                        "product_name": "7200120090001",
+                        "customer_type": "0702",
+                        "accounting_status": "active",
+                        "accounting_type": "PRINL",
+                        "business_product": "00000001",
+                        "business_area": "1111111111",
+                        "profit_center": "0000001212",
+                        "service": "disbursement",
+                        "other_prop": {
+                            "gl_entries_other_properties": {
+                                "trnRef": "4S8UG08LOtEiLk--0-1F-Go-,0"
+                            },
+                            "trn_entries_other_properties": {
+                                "before_balance": "0.00"
+                            },
+                            "trn_other_properties": {
+                                "first_payment_date": "2019-08-31",
+                                "installment_amount": "20483.56",
+                                "number_of_payment": "5"
+                            }
+                        },
+                        "chrono_sequence": "1911061451203395102372256"
+                    }
+                    // },
+                //     {
+                //         "transaction_id": "4S8UG08LOtEiLk--0-1F-Go-2",
+                //         "transaction_sequence": 2,
+                //         "transaction_datetime": "2019-10-30T15:26:05+07:00",
+                //         "transaction_date": "2019-10-30",
+                //         "channel_post_date": "2019-08-25",
+                //         "datetime_stamp": "2019-11-06T14:51:20+07:00",
+                //         "job_id": "762c31b2-359b-4301-b864-26bf9e53cdde",
+                //         "account_branch": 1,
+                //         "service_branch": 1,
+                //         "cost_center": "5",
+                //         "transaction_account_type": "GL",
+                //         "dr_cr": "C",
+                //         "gl_account_number": 2731111,
+                //         "transaction_amount": 100000.00,
+                //         "currency": "THB",
+                //         "base_amount": 100000.00,
+                //         "base_currency": "THB",
+                //         "clearing_and_settlement_key": "CBS",
+                //         "business_product": "00000000",
+                //         "business_area": "5555555555",
+                //         "profit_center": "0000006060",
+                //         "service": "disbursement",
+                //         "other_prop": {
+                //             "gl_entries_other_properties": {
+                //                 "trnRef": "4S8UG08LOtEiLk--0-1F-Go-,1"
+                //             },
+                //             "trn_other_properties": {
+                //                 "first_payment_date": "2019-08-31",
+                //                 "installment_amount": "20483.56",
+                //                 "number_of_payment": "5"
+                //             }
+                //         },
+                //         "chrono_sequence": "1911061451203418592812256"
+                //     },
+                //     {
+                //         "transaction_id": "4S8UG08LOtEiLk--0-1F-Go-3",
+                //         "transaction_sequence": 3,
+                //         "transaction_datetime": "2019-10-30T15:26:05+07:00",
+                //         "transaction_date": "2019-10-30",
+                //         "channel_post_date": "2019-08-25",
+                //         "datetime_stamp": "2019-11-06T14:51:20+07:00",
+                //         "job_id": "762c31b2-359b-4301-b864-26bf9e53cdde",
+                //         "account_branch": 1,
+                //         "service_branch": 1,
+                //         "cost_center": "1",
+                //         "transaction_account_type": "ITC",
+                //         "dr_cr": "C",
+                //         "gl_account_number": 1110000,
+                //         "transaction_amount": 100000.00,
+                //         "currency": "THB",
+                //         "base_amount": 100000.00,
+                //         "base_currency": "THB",
+                //         "business_product": "00000000",
+                //         "business_area": "1111111111",
+                //         "profit_center": "0000001212",
+                //         "service": "disbursement",
+                //         "chrono_sequence": "1911061451203395102372256"
+                //     },
+                //     {
+                //         "transaction_id": "4S8UG08LOtEiLk--0-1F-Go-4",
+                //         "transaction_sequence": 4,
+                //         "transaction_datetime": "2019-10-30T15:26:05+07:00",
+                //         "transaction_date": "2019-10-30",
+                //         "channel_post_date": "2019-08-25",
+                //         "datetime_stamp": "2019-11-06T14:51:20+07:00",
+                //         "job_id": "762c31b2-359b-4301-b864-26bf9e53cdde",
+                //         "account_branch": 1,
+                //         "service_branch": 1,
+                //         "cost_center": "5",
+                //         "transaction_account_type": "ITC",
+                //         "dr_cr": "D",
+                //         "gl_account_number": 1110000,
+                //         "transaction_amount": 100000.00,
+                //         "currency": "THB",
+                //         "base_amount": 100000.00,
+                //         "base_currency": "THB",
+                //         "business_product": "00000000",
+                //         "business_area": "5555555555",
+                //         "profit_center": "0000006060",
+                //         "service": "disbursement",
+                //         "chrono_sequence": "1911061451203418592812256"
+                //     }
+                // ],
+                // "has_more": false,
+                // "last_index": "4"
+                ]
+            }
+        }
+
+        if (data.rs_body) {
+            utility.clearSessionStorage("response_inquiryAccountingRecord");
+            sessionStorage.setItem("response_inquiryAccountingRecord", JSON.stringify(data.rs_body.gl_entry_list));
+            window.open('/iarSummary', '_self');
+        } else {
+            alert("error code : " + data.errors.map(error => error.error_code) + "\n"
+                + "error desc : " + data.errors.map(error => error.error_desc) + "\n"
+                + "error type : " + data.errors.map(error => error.error_type));
+        }
     };
 
     FormInputData = () => {
--- client/src/App/pages/inqAccountingRecords/InquiryAccountingRecordsSummery.js
+++ client/src/App/pages/inqAccountingRecords/InquiryAccountingRecordsSummery.js
@@ -2,85 +2,91 @@ import React, { Component } from 'react';
 import { Col, Form, Table } from 'reactstrap';
 import DynamicHeader from '../Header.js';
 // import utility from '../Utility.js';
+import Modal from '../modal'
+import fieldHeader from './fieldRes.js';
 
 class inquiryAccountingRecordsSummery extends Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            status: false
+        };
         this.searchJobID = this.searchJobID.bind(this);
     }
 
-    getHeaderTable = (data) => {
+    getHeaderTable = () => {
         let header = [];
-        if (data.length > 1) {
-            header.push(<th>#&nbsp;</th>);
-            // eslint-disable-next-line
-            for (let key in data[0]) {
-                if (data[0].hasOwnProperty(key)) {
-                    if (typeof data[0][key] === "object") {
-                        // eslint-disable-next-line
-                        for (let inObj in data[0][key]) {
-                            header.push(<th><u>{inObj}</u>&nbsp;</th>);
-                        }
-                    } else {
-                        header.push(<th>{key}&nbsp;</th>);
-                    }
-                }
+        header.push(<th>#&nbsp;</th>);
+        fieldHeader.gl_entry_list.map(item => {
+            if (item === "trnRef" || item === "before_balance" || item === "first_payment_date"
+                || item === "installment_amount" || item === "number_of_payment") {
+                header.push(<th><u>{item}</u>&nbsp;</th>);
+            } else {
+                header.push(<th>{item}&nbsp;</th>);
             }
-        } else {
-            // eslint-disable-next-line
-            for (let key in data) {
-                if (data.hasOwnProperty(key)) {
-                    header.push(<th>#&nbsp;</th>);
-                    // eslint-disable-next-line
-                    for (let keyinObj in data[key]) {
-                        if (data[key].hasOwnProperty(keyinObj)) {
-                            if (typeof data[key][keyinObj] === "object") {
-                                // eslint-disable-next-line
-                                for (let inObj in data[key][keyinObj]) {
-                                    header.push(<th><u>{inObj}</u>&nbsp;</th>);
-                                }
-                            } else {
-                                header.push(<th>{keyinObj}&nbsp;</th>);
-                            }
-                        }
-                    }
-                }
-            }
-        }
+        })
         return header;
     };
 
     getBodyTable = (data) => {
         let body = [];
-        // eslint-disable-next-line
-        for (let key in data) {
-            if (data.hasOwnProperty(key)) {
-                let num = Number(key);
-                let obj = [];
-                let dataByRow = [];
-                dataByRow.push(num + 1);
-                obj.push(<td>{num + 1}</td>);
-                // eslint-disable-next-line
-                for (let keyinObj in data[key]) {
-                    if (data[key].hasOwnProperty(keyinObj)) {
-                        if (typeof data[key][keyinObj] === "object") {
-                            // eslint-disable-next-line
-                            for (let inObj in data[key][keyinObj]) {
-                                obj.push(<td>{data[key][keyinObj][inObj]}&nbsp;</td>);
-                                dataByRow.push(data[key][keyinObj][inObj]);
+        for (let index in data) {                         // length index in json response 
+            let obj = [];
+            let num = Number(index) + 1;
+            obj.push(<td>{num}</td>);
+
+            // if (data.hasOwnProperty(index)) {
+            // eslint-disable-next-line
+                 for (let ResHeader in data[index]) {
+                    if (typeof data[index][ResHeader] === "object") {
+                        for(let inObj in data[index][ResHeader]){
+                            for(let keyInObj in  data[index][ResHeader][inObj]){
+                                // if(item === keyInObj){
+                                //         obj.push(<td>{data[index][ResHeader][inObj][keyInObj]}</td>);
+                                //         break;
+                                //     }
+                                // this.checkFieldHeader(keyInObj);
+                                // console.log("obj ="+this.checkFieldHeader(keyInObj));
+                                this.checkFieldHeader(keyInObj);
                             }
-                        } else {
-                            obj.push(<td>{data[key][keyinObj]}</td>);
-                            dataByRow.push(data[key][keyinObj]);
                         }
+                     } else {
+                        //  console.log("not = "+ResHeader);
+                        // if(ResHeader === item){
+                        //     obj.push(<td>{data[index][ResHeader]}</td>);
+                        //     break;
+                        // }
+                         
+                        //   console.log(this.checkFieldHeader(ResHeader));
+                        this.checkFieldHeader(ResHeader);
+
+
                     }
-                }
-                body.push(<tr onClick={() => this.searchJobID(dataByRow)}>{obj}</tr>);
-            }
+                 }
+            // body.push(<tr>{obj}</tr>);
         }
         return body;
     };
+
+    checkFieldHeader = (field) => {
+        // console.log(field);
+        let key = []
+        fieldHeader.gl_entry_list.map(item => {  
+            // if(item.hasOwnProperty(field)){
+            //     console.log("then ="+item);
+            // }else{
+            //     console.log("else="+item);
+            // }
+            // if(field === item){
+            //      key.push(field);
+            // }
+        })
+         return key;
+    };
+
+    showModal = e => {
+        this.setState({ status: !this.state.status });
+    }
 
     searchJobID = (value) => {
         console.log(value);
@@ -91,47 +97,47 @@ class inquiryAccountingRecordsSummery extends Component {
         //         .then(data => {
         //             if (data.rs_body) {
         //                 return(
-            // let data = {
-            //     "rs_body": {
-            //         "gl_entry_list": [
-            //             {
-            //                 "transaction_id": "TestFind",
-            //                 "transaction_sequence": 1,
-            //                 "transaction_datetime": "2019-11-01T10:59:01Z",
-            //                 "transaction_date": "2019-11-01",
-            //                 "channel_post_date": "2019-11-01",
-            //                 "datetime_stamp": "2019-11-01T10:59:01Z",
-            //                 "job_id": "001",
-            //                 "account_branch": 1,
-            //                 "service_branch": 1,
-            //                 "cost_center": "5555555555",
-            //                 "transaction_account_type": "ITC",
-            //                 "dr_cr": "C",
-            //                 "gl_account_number": 2000000001,
-            //                 "before_prin_balance": 77662796314522418.32,
-            //                 "transaction_amount": 77662796314522418.32,
-            //                 "after_prin_balance": 77662796314522418.32,
-            //                 "currency": "THB",
-            //                 "base_amount": 77662796314522418.32,
-            //                 "base_currency": "THB",
-            //                 "accounting_type": "ACCPEN",
-            //                 "business_product": "00000002",
-            //                 "business_area": "5555555555",
-            //                 "profit_center": "0000006060",
-            //                 "service": "test",
-            //                 "other_properties": {
-            //                     "interest_index": "test1",
-            //                     "interest_spread": 2,
-            //                     "first_payment_date": "test2",
-            //                 },
-            //                 "chrono_sequence": "1234567890"
-            //             }
-            //         ]
-            //     }
-            // };
-            //                 utility.clearSessionStorage("response_inquiryAccountingByRow");
-            //                 sessionStorage.setItem("response_inquiryAccountingByRow", JSON.stringify(data.rs_body.gl_entry_list));
-            //                 window.open('/iabjSummary');
+        // let data = {
+        //     "rs_body": {
+        //         "gl_entry_list": [
+        //             {
+        //                 "transaction_id": "TestFind",
+        //                 "transaction_sequence": 1,
+        //                 "transaction_datetime": "2019-11-01T10:59:01Z",
+        //                 "transaction_date": "2019-11-01",
+        //                 "channel_post_date": "2019-11-01",
+        //                 "datetime_stamp": "2019-11-01T10:59:01Z",
+        //                 "job_id": "001",
+        //                 "account_branch": 1,
+        //                 "service_branch": 1,
+        //                 "cost_center": "5555555555",
+        //                 "transaction_account_type": "ITC",
+        //                 "dr_cr": "C",
+        //                 "gl_account_number": 2000000001,
+        //                 "before_prin_balance": 77662796314522418.32,
+        //                 "transaction_amount": 77662796314522418.32,
+        //                 "after_prin_balance": 77662796314522418.32,
+        //                 "currency": "THB",
+        //                 "base_amount": 77662796314522418.32,
+        //                 "base_currency": "THB",
+        //                 "accounting_type": "ACCPEN",
+        //                 "business_product": "00000002",
+        //                 "business_area": "5555555555",
+        //                 "profit_center": "0000006060",
+        //                 "service": "test",
+        //                 "other_properties": {
+        //                     "interest_index": "test1",
+        //                     "interest_spread": 2,
+        //                     "first_payment_date": "test2",
+        //                 },
+        //                 "chrono_sequence": "1234567890"
+        //             }
+        //         ]
+        //     }
+        // };
+        //                 utility.clearSessionStorage("response_inquiryAccountingByRow");
+        //                 sessionStorage.setItem("response_inquiryAccountingByRow", JSON.stringify(data.rs_body.gl_entry_list));
+        //                 window.open('/iabjSummary');
         //                 );
         //             } else {
         //                 alert("error code : " + data.errors.map(error => error.error_code) + "\n"
@@ -145,29 +151,30 @@ class inquiryAccountingRecordsSummery extends Component {
         const data = JSON.parse(sessionStorage.getItem("response_inquiryAccountingRecord"));
         //console.log(data);
         return (
-                <div className="App">
-                    <DynamicHeader />
-                    <Form>
-                        <h2>Form Data Inquiry Accounting Record</h2>
-                        <Col md={{ size: 10, offset: 1 }}>
-                            <div class="table-responsive">
-                                <Table hover>
-                                    <thead>
-                                        <tr>
-                                            {this.getHeaderTable(data)}
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        {this.getBodyTable(data)}
-                                    </tbody>
-                                </Table>
-                                <div>
+            <div className="App">
+                <DynamicHeader />
+                <Form>
+                    <h2>Form Data Inquiry Accounting Record</h2>
+                    <Col md={{ size: 10, offset: 1 }}>
+                        <div class="table-responsive">
+                            <Table hover>
+                                <thead>
+                                    <tr>
+                                        {this.getHeaderTable()}
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {this.getBodyTable(data)}
+                                </tbody>
+                            </Table>
+                            <div>
                             </div>
                         </div>
                     </Col>
                 </Form>
+                <Modal show={this.state.status} onClose={this.showModal} />
             </div>
-         );
+        );
     }
 }
 export default inquiryAccountingRecordsSummery;
