7e79a1373f744fbd63869868f8924a544ddf6f73
PopatY <maxbaraja_max@hotmail.com>
2020-01-27 10:34:34 +0700

    update disburse properties

--- client/src/App/pages/disbursement/DisbursementComponent.js
+++ client/src/App/pages/disbursement/DisbursementComponent.js
@@ -1,13 +1,24 @@
-import React, { Component } from 'react';
-import { Button, Col, Container, Form, FormGroup, Input, Label, Row } from 'reactstrap';
+import React, { Component, useState } from 'react';
+import { Table, Button, Col, Container, Form, FormGroup, Input, Label, Row } from 'reactstrap';
 import DynamicHeader from '../Header.js';
 import inputModel from './model.json';
 import utility from '../Utility.js';
 import SpinnerLoader from '../loading.js';
+import ReactModal from 'react-modal';
 
 const cloneDeep = require('lodash.clonedeep');
 let installmentAmount = "";
 let numberOfPayment = "";
+const customStyles = {
+    content : {
+        top                   : '50%',
+        left                  : '50%',
+        right                 : 'auto',
+        bottom                : 'auto',
+        marginRight           : '-50%',
+        transform             : 'translate(-50%, -50%)'
+    }
+};
 
 class disbursementComponent extends Component {
     constructor(props) {
@@ -27,19 +38,38 @@ class disbursementComponent extends Component {
                     first_payment_date: "",
                     number_of_payment: 0,
                     installment_amount: 0,
-                    payment_calculation_method: ""
+                    payment_calculation_method: "",
+                    interest_override_reason:"",
+                    campaign_name:"",
+                    interest_schedule:[]
                 }
             },
             loading: false,
-            disabled: ""
-        }
+            disabled: "",
+            rows:[],
+            showModal:false,
+            date:"",
+            interest_index:"",
+            interest_spread:"",
+            // myArray : [{"date":"2020-01-01","interest_index":"1","interest_spread":"1"}]
+            myArray : []
+
+        };
         this.handleChange = this.handleChange.bind(this);
+        this.handleChangeTest = this.handleChangeTest.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleOpenModal = this.handleOpenModal.bind(this);
+        this.handleCloseModal = this.handleCloseModal.bind(this);
+        this.closeModal = this.closeModal.bind(this);
     };
 
     componentDidMount() {
-        let disbursementAmount = 0; 
+        let disbursementAmount = 0;
         let accountNumber = "";
+
+        if((JSON.parse(sessionStorage.getItem("disburse_interest")))){
+            sessionStorage.removeItem("disburse_interest")
+        }
 
         if (JSON.parse(sessionStorage.getItem("response_installment"))) {
             const dataInstallment = JSON.parse(sessionStorage.getItem("response_installment"));
@@ -78,9 +108,20 @@ class disbursementComponent extends Component {
                 installment_amount:String(installmentAmount),
                 payment_calculation_method: "installment"
             }
-        }
+        };
         this.setState({ rq_body: body });
     };
+
+    handleChangeTest(event){
+        console.log(event.target.value);
+        if(event.target.name === "date"){
+            this.setState({date : event.target.value});
+        }else if(event.target.name === "interest_index"){
+            this.setState({interest_index : event.target.value})
+        }else if(event.target.name === "interest_spread"){
+            this.setState({interest_spread : event.target.value});
+        }
+    }
 
     handleChange(event) {
         //this.setState({[event.target.name]:event.target.value});
@@ -89,8 +130,9 @@ class disbursementComponent extends Component {
         const properties = currentState.other_properties;
         if (event.target.name === "interest_index" || event.target.name === "interest_spread"
             || event.target.name === "first_payment_date" || event.target.name === "number_of_payment"
-            || event.target.name === "installment_amount") {
-            
+            || event.target.name === "installment_amount" || event.target.name === "interest_override_reason"
+            || event.target.name === "campaign_name"){
+
             properties[event.target.name] = event.target.value;
 
         } else if (event.target.name === "payment_calculation_method"){
@@ -105,13 +147,26 @@ class disbursementComponent extends Component {
                 properties["number_of_payment"] = String(numberOfPayment);
                 this.setState({disabled : ""});
             }
-        }else{
+        }
+        else{
             currentState[event.target.name] = event.target.type === "number" ? Number(event.target.value) : event.target.value;
         }
         this.setState({ rq_body: currentState });
     };
 
     handleSubmit(event) {
+
+        console.log(sessionStorage.getItem("disburse_interest"));
+
+        //set Array for interest schedule
+        const { rq_body } = { ...this.state };
+        const currentState = rq_body;
+        const properties = currentState.other_properties;
+        properties["interest_schedule"] = JSON.parse(sessionStorage.getItem("disburse_interest"));
+
+
+        // this.setState({ rq_body[""]["interest_schedule"] : JSON.parse(sessionStorage.getItem("disburse_interest"))});
+
         event.preventDefault();
         this.setState({ loading: true });
         //clone state for use in omit function.
@@ -156,6 +211,74 @@ class disbursementComponent extends Component {
         //     + "error desc : " + data.errors.map(error => error.error_desc) + "\n"
         //     + "error type : " + data.errors.map(error => error.error_type));
         // }
+    };
+
+    handleOpenModal () {
+        this.setState({ showModal: true });
+    }
+
+    closeModal () {
+
+        this.setState({ showModal: false });
+    }
+
+    handleCloseModal () {
+
+        this.setState({ showModal: false });
+
+        var dataArray = [];
+        var data = {
+            date : this.state.date,
+            interest_index : this.state.interest_index,
+            interest_spread : this.state.interest_spread
+        };
+        console.log("DATA:"+ JSON.stringify(data));
+
+        // sessionStorage.clear();
+        if(sessionStorage.getItem("disburse_interest") != null){
+            dataArray = JSON.parse(sessionStorage.getItem("disburse_interest"));
+            console.log("getget ="+dataArray);
+            dataArray.push(data);
+
+            for (var i = 0; i < dataArray.length; i++)
+                console.log((i+1) + ": " + JSON.stringify(dataArray[i]));
+
+            sessionStorage.setItem("disburse_interest", JSON.stringify(dataArray));
+        } else{
+            dataArray.push(data);
+            sessionStorage.setItem("disburse_interest", JSON.stringify(dataArray));
+            console.log(dataArray);
+        }
+
+        //[{"date":"2020-01-23","interest_index":"1","interest_spread":"1"}]
+
+        this.renderTable();
+
+        //
+        // this.setState({date : ""});
+        // this.setState({interest_index : ""});
+        // this.setState({interest_spread : ""});
+        // this.renderTableData()
+    }
+
+    renderTable() {
+        if(sessionStorage.getItem("disburse_interest") != null){
+            return
+        }
+    }
+
+
+    handleAddRow = () => {
+        this.setState((prevState, props) => {
+            const row = { content: "hello this is a new row!" };
+            return { rows: [...prevState.rows, row] };
+        });
+    };
+
+    handleRemoveRow = () => {
+        this.setState((prevState, props) => {
+            return { rows: prevState.rows.slice(1) };
+        });
     };
 
     FormInputData = () => {
@@ -203,6 +326,20 @@ class disbursementComponent extends Component {
         })
     };
 
+    renderTableData() {
+        return this.state.myArray.map((arr, index) => {
+            const { date, interest_index, interest_spread } = arr; //destructuring
+            return (
+                <tr key={index}>
+                    <td>{index}</td>
+                    <td>{date}</td>
+                    <td>{interest_index}</td>
+                    <td>{interest_spread}</td>
+                </tr>
+            )
+        })
+    }
+
     render() {
         const { loading } = this.state;
         return (
@@ -214,8 +351,36 @@ class disbursementComponent extends Component {
                         <Row>
                             <Col md={{ size: 4, offset: 4 }}>
                                 {this.FormInputData()}
+                                <FormGroup>
+                                    <Label>Interest Schedule</Label>
+                                    <div>
+                                        <Button color="secondary" type="button" onClick={this.handleOpenModal}>Add Interest Schedule</Button>
+                                        <ReactModal
+                                            isOpen={this.state.showModal}
+                                            style = {customStyles}
+                                            contentLabel="Interest Schedule">
+                                            <FormGroup>
+                                                <Label>Date</Label>
+                                                <Input type="String" name="date" placeholder="date" step="any"
+                                                       value={this.state.date} onChange={this.handleChangeTest} >
+                                                </Input>
+                                                <Label>Interest Index</Label>
+                                                <Input type="String" name="interest_index" placeholder="interest_index" step="any"
+                                                       value={this.state.interest_index} onChange={this.handleChangeTest} >
+                                                </Input>
+                                                <Label>Interest Spread</Label>
+                                                <Input type="number" name="interest_spread" placeholder="interest_spread" step="any"
+                                                       value={this.state.interest_spread} onChange={this.handleChangeTest} >
+                                                </Input>
+                                                <button type="button" onClick={this.handleCloseModal}>Add</button>
+                                                <button type="button" onClick={this.closeModal}>close</button>
+                                            </FormGroup>
+                                        </ReactModal>
+                                    </div>
+                                </FormGroup>
                             </Col>
                         </Row>
+                        {this.renderTable()}
                         <div class="text-center">
                             <Button color="primary" type="submit" disabled={loading}>
                                 {loading && (<SpinnerLoader />)}
@@ -224,10 +389,80 @@ class disbursementComponent extends Component {
                             </Button>
                         </div>
                     </Form>
+                    <Row>
+                        <Col md={{ size: 4, offset: 4 }}>
+
+                        </Col>
+                    </Row>
+                    {/**/}
+
+                    {/*<Button color="danger" type="" onClick={this.clickMe()}></Button>*/}
+                    {/*<div>*/}
+                    {/*    <Table>*/}
+                    {/*        <tbody>*/}
+                    {/*        {this.state.rows.map(row => (*/}
+                    {/*            <tr>*/}
+                    {/*                <td>{row.content}</td>*/}
+                    {/*            </tr>*/}
+                    {/*        ))}*/}
+                    {/*        <tr>*/}
+                    {/*            <td className="" onClick={this.handleAddRow}>*/}
+                    {/*                (+)*/}
+                    {/*            </td>*/}
+                    {/*            {Boolean(this.state.rows.length) && (*/}
+                    {/*                <td onClick={this.handleRemoveRow}>(-)</td>*/}
+                    {/*            )}*/}
+                    {/*        </tr>*/}
+                    {/*        </tbody>*/}
+                    {/*    </Table>*/}
+                    {/*</div>*/}
                 </Container>
             </div>
         )
     };
+
+    // clickMe() {
+    //     return function (p1) {
+    //         // alert("Hello World")
+    //         <div>
+    //             <h1>hello world</h1>
+    //         </div>
+    //     };
+    // }
+
+    // clickMe = () => {
+    //     return <Table hover>
+    //         <thead>
+    //         <tr>
+    //             <th>#</th>
+    //             <th>Date</th>
+    //             <th>Interest Index</th>
+    //             <th>Interest Spread</th>
+    //         </tr>
+    //         </thead>
+    //         <tbody>
+    //         <tr>
+    //             <th scope="row">1</th>
+    //             <td>Mark</td>
+    //             <td>Otto</td>
+    //             <td>@mdo</td>
+    //         </tr>
+    //         <tr>
+    //             <th scope="row">2</th>
+    //             <td>Jacob</td>
+    //             <td>Thornton</td>
+    //             <td>@fat</td>
+    //         </tr>
+    //         <tr>
+    //             <th scope="row">3</th>
+    //             <td>Larry</td>
+    //             <td>the Bird</td>
+    //             <td>@twitter</td>
+    //         </tr>
+    //         </tbody>
+    //     </Table>
+    // }
+
 }
 
 export default disbursementComponent;
