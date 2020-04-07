0a0ac0e7ecb701bfd95ed9d8bf76b7c44dda766d
petchza <anon.phaoinjun@gmail.com>
2020-04-07 19:48:22 +0700

    Update code dynamic response at disbursement and pre-disbursement

--- client/src/App/pages/disbursement/DisbursementSummery.js
+++ client/src/App/pages/disbursement/DisbursementSummery.js
@@ -58,47 +58,6 @@ class disbursementSummery extends Component {
     // }).catch(error => console.log(error))
     //};
 
-    // dynamicResponse = (data) => {
-    //     let table = [];
-    //     let children = [];
-    //     // eslint-disable-next-line
-    //     for (let key in data) {
-    //         if (data.hasOwnProperty(key)) {
-    //             if (typeof data[key] === "object") {
-    //                 let obj = [];
-    //                 // eslint-disable-next-line
-    //                 for (let subdata in data[key]) {
-    //                     if (data[key].hasOwnProperty(subdata)) {
-    //                         if (typeof data[key][subdata] === "boolean") {
-    //                             let catchup = String(data[key][subdata]);
-    //                             obj.push(<tr>
-    //                                 <td>{subdata}</td>
-    //                                 <td>{catchup}</td>
-    //                             </tr>)
-    //                         } else {
-    //                             obj.push(<tr>
-    //                                 <td>{subdata}</td>
-    //                                 <td>{data[key][subdata]}</td>
-    //                             </tr>);
-    //                         }
-    //                     }
-    //                 }
-    //                 children.push(<tr>
-    //                     <td>{key + " : "}</td>
-    //                     <td><Table>{obj}</Table></td>
-    //                 </tr>);
-    //             } else {
-    //                 children.push(<tr>
-    //                     <td>{key}</td>
-    //                     <td>{data[key]}</td>
-    //                 </tr>);
-    //             }
-    //         }
-    //     }
-    //     table.push(<Table bordered>{children}</Table>);
-    //     return table;
-    // };
-
     dynamicResponse = (data) => {
         let table = [];
         let children = [];
@@ -117,27 +76,10 @@ class disbursementSummery extends Component {
                                     <td>{catchup}</td>
                                 </tr>)
                             } else {
-                                let keyList = [];
-                                //otherproperties;
-                                if(typeof data[key][subdata] === "object"){
-                                    for (let element in data[key][subdata]) {
-                                        for (let keyElement in data[key][subdata][element]) {
-                                            keyList.push(<tr>
-                                                <td>{keyElement}</td>
-                                                <td>{data[key][subdata][element][keyElement]}</td>
-                                            </tr>);
-                                        }
-                                    }
-                                    obj.push(<tr>
-                                         <td>{subdata + " : "}</td>
-                                         <td><Table>{keyList}</Table></td>
-                                    </tr>);
-                                }else{
-                                     obj.push(<tr>
-                                        <td>{subdata}</td>
-                                        <td>{data[key][subdata]}</td>
-                                    </tr>);
-                                }
+                                obj.push(<tr>
+                                    <td>{subdata}</td>
+                                    <td>{data[key][subdata]}</td>
+                                </tr>);
                             }
                         }
                     }
